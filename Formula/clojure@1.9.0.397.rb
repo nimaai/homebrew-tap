3bf66cd6da0b80a11bd50c7a19fecf09db685941
Matus Kmit <matus.kmit@zhdk.ch>
2021-01-08 13:44:30 +0100

    clojure@1.9.0.397

---
+++ Formula/clojure@1.9.0.397.rb
@@ -0,0 +1,23 @@
+class ClojureAT190397 < Formula
+  desc "The Clojure Programming Language"
+  homepage "https://clojure.org"
+  url "https://download.clojure.org/install/clojure-tools-1.9.0.397.tar.gz"
+  sha256 "5ed9b1d783560d06b3358a2d809fb0ab599d9ed6034919bf76c46e5eeea1a982"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+  depends_on "rlwrap"
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system("#{bin}/clj -e nil")
+    %w[clojure clj].each do |clj|
+      assert_equal "2", shell_output("#{bin}/#{clj} -e \"(+ 1 1)\"").strip
+    end
+  end
+end
